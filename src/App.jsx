90f32f27f103c0e1ab1f3c175cdb16c2601f75bf
cx-keval <keval.culturex@gmail.com>
2024-09-27 21:17:22 +0530

    FEAT: page 4

--- src/App.jsx
+++ src/App.jsx
@@ -25,6 +25,24 @@ const App = () => {
             <PageWrapper>
                 <PageFour />
             </PageWrapper>
+
+            <SegmentStarterPage
+                index={2}
+                title="Engagement Diagnosis"
+                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exer"
+                list={[
+                    <>
+                        <strong>Feed Engagement</strong> (Overview, Posting
+                        Frequency, Recent Feed)
+                    </>,
+                    <>
+                        <strong>Reels Engagement</strong> (Overview, Posting
+                        Frequency, Recent Reels)
+                    </>,
+                    <strong>Growth Rate</strong>,
+                    <strong>Sentiment Analysis</strong>,
+                ]}
+            />
         </div>
     );
 };
