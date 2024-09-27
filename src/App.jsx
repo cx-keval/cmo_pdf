c4d9b97d378dee4b779848c12ecb9cbe03335c3a
cx-keval <keval.culturex@gmail.com>
2024-09-27 21:12:49 +0530

    FEAT: reusable SegmentStarterPage component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
-import PageTwo from "./components/PageTwo";
 import PageThree from "./components/PageThree";
 import PageFour from "./components/PageFour";
 import PageWrapper from "./components/PageWrapper";
+import SegmentStarterPage from "./components/SegmentStarterPage";
 
 const App = () => {
     return (
         <div aria-label="main" className="w-full">
-            <PageTwo />
+            <SegmentStarterPage
+                index={1}
+                title="Profile Diagnosis"
+                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exer"
+                list={[
+                    "Profile",
+                    "Profile Metrics",
+                    "Profile vs Competitor",
+                    "Social Overview",
+                ]}
+            />
 
             <PageWrapper>
                 <PageThree />
--- src/components/SegmentStarterPage.jsx
+++ src/components/SegmentStarterPage.jsx
@@ -4,7 +4,12 @@ import {
     InfluenzerLighteningSVG,
 } from "./SVGs";
 
-const PageOne = () => {
+const SegmentStarterPage = ({
+    title = "",
+    description = "",
+    index = 1,
+    list = [],
+}) => {
     return (
         <section className="relative mx-auto flex aspect-[841/594] h-screen w-auto flex-col overflow-hidden border">
             <InfluenzerLighteningSVG className="absolute left-20 top-0" />
@@ -12,24 +17,26 @@ const PageOne = () => {
             <BottomLeftDotsSVG className="absolute -left-0 bottom-0 h-[160px] w-auto" />
 
             <div className="mx-auto flex w-3/5 flex-1 flex-col items-center justify-center">
-                <h1 className="text-5xl font-medium text-slate-900">
-                    Profile Diagnosis
+                <div className="flex items-center justify-center">
+                    <span className="flex h-[5rem] w-[5rem] items-center justify-center rounded-full bg-slate-50 text-4xl font-semibold text-slate-600">
+                        {index}
+                    </span>
+                </div>
+                <h1 className="mt-4 text-5xl font-medium text-slate-900">
+                    {title}
                 </h1>
                 <p className="mt-4 text-center text-xl text-slate-400">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis nostrud exer
+                    {description}
                 </p>
                 <hr className="mt-7 w-full border-slate-200" />
                 <ul className="mt-7 grid w-full list-disc grid-cols-2 gap-[1rem_2rem] rounded-xl bg-slate-50 p-8 pl-14 text-xl text-slate-600">
-                    <li>Profile</li>
-                    <li>Profile Metrics</li>
-                    <li>Profile vs Competitor</li>
-                    <li>Social Overview</li>
+                    {list.map((item) => (
+                        <li>{item}</li>
+                    ))}
                 </ul>
             </div>
         </section>
     );
 };
 
-export default PageOne;
+export default SegmentStarterPage;
