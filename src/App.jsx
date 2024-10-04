04662bdd411bfe3d08db188eb537436af79f3ffe
cx-keval <keval.culturex@gmail.com>
2024-10-04 17:59:19 +0530

    FEAT: page Thank You

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import PageThirteen from "./components/PageThirteen";
 import PageFourteen from "./components/PageFourteen";
 import PageFifteen from "./components/PageFifteen";
 import PageSixteen from "./components/PageSixteen";
+import ThankYou from "./components/ThankYou";
 
 const App = () => {
     return (
@@ -69,6 +70,8 @@ const App = () => {
             <PageWrapper>
                 <PageSixteen />
             </PageWrapper>
+
+            <ThankYou />
         </div>
     );
 };
